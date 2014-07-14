dc20145b34dea4ae0c61875e815c59ddb81805d7
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-14 15:24:42 +0300

    Add basic structure for starting and stopping service

--- app/src/main/java/gs/meetin/connector/ConnectorActivity.java
+++ app/src/main/java/gs/meetin/connector/ConnectorActivity.java
@@ -1,9 +1,15 @@
 package gs.meetin.connector;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ConnectorActivity extends ActionBarActivity {
@@ -12,6 +18,31 @@ public class ConnectorActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connector);
+
+        final Context ctx = getApplicationContext();
+
+        final Intent intent = new Intent(ctx, ConnectorService.class);
+
+        Button startService = (Button) findViewById(R.id.buttonStartService);
+        Button stopService = (Button) findViewById(R.id.buttonStopService);
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "service starting", Toast.LENGTH_SHORT).show();
+
+                ctx.startService(intent);
+            }
+        });
+
+        stopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "service stopping", Toast.LENGTH_SHORT).show();
+
+                ctx.stopService(intent);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/gs/meetin/connector/ConnectorService.java
@@ -0,0 +1,51 @@
+package gs.meetin.connector;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ConnectorService extends IntentService {
+
+    private boolean running = false;
+
+    public ConnectorService() {
+        super("ConnectorService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("Mtn.gs", "Starting service...");
+        running = true;
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("Mtn.gs", "Stopping service...");
+        running = false;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        int i = 0;
+        while (running) {
+            synchronized (this) {
+                try {
+                    Log.d("Mtn.gs", "Working... " + i);
+                    i++;
+                    wait(1000);
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+}
