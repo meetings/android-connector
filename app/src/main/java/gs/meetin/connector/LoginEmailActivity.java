734f913a054614d6275f2ca73f424ae9633d1612
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-15 10:26:57 +0300

    Validate email in login activity

--- app/src/main/java/gs/meetin/connector/LoginEmailActivity.java
+++ app/src/main/java/gs/meetin/connector/LoginEmailActivity.java
@@ -1,9 +1,12 @@
 package gs.meetin.connector;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,15 +25,6 @@ public class LoginEmailActivity extends ActionBarActivity {
         setButtonListeners();
     }
 
-    protected void showPinRequest() {
-        String email = ((EditText) findViewById(R.id.inputEmail)).getText().toString();
-
-        Intent loginIntent = new Intent(this, LoginPinActivity.class);
-        loginIntent.putExtra(EXTRA_EMAIL, email);
-        startActivity(loginIntent);
-
-    }
-
     private void setButtonListeners() {
         Button sendEmail = (Button) findViewById(R.id.buttonSignInEmail);
 
@@ -40,5 +34,38 @@ public class LoginEmailActivity extends ActionBarActivity {
                 showPinRequest();
             }
         });
+    }
+
+    private void showPinRequest() {
+        String email = ((EditText) findViewById(R.id.inputEmail)).getText().toString();
+
+        if (isValidEmail(email)) {
+            Intent loginIntent = new Intent(this, LoginPinActivity.class);
+            loginIntent.putExtra(EXTRA_EMAIL, email);
+            startActivity(loginIntent);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage(R.string.invalid_email_message)
+                   .setTitle(R.string.invalid_email_title)
+                   .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialogInterface, int i) {
+                           dialogInterface.dismiss();
+                       }
+                   });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    private boolean isValidEmail(CharSequence email) {
+        if (email == null) {
+            return false;
+        }
+
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
     }
 }
