d1e6f6e430a99ce7828e68045193ba1cba3ac71b
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-17 15:16:50 +0300

    Simplify login dtos

--- app/src/main/java/gs/meetin/connector/dto/LoginRequest.java
+++ app/src/main/java/gs/meetin/connector/dto/LoginRequest.java
@@ -2,23 +2,21 @@ package gs.meetin.connector.dto;
 
 public class LoginRequest {
 
-    public String email;
-    public String pin;
+    // Repsonse
+    public User result;
+    public ApiError error;
+
+    // Request
+    private String email;
+    private String pin;
 
     public LoginRequest(String email, String pin) {
         this.email = email;
         this.pin = pin;
     }
 
-    public class LoginResult {
-        public User result;
-        public ApiError error;
-    }
-
     public class User {
         public String user_id;
         public String token;
     }
-
-
 }
--- app/src/main/java/gs/meetin/connector/dto/PinRequest.java
+++ app/src/main/java/gs/meetin/connector/dto/PinRequest.java
@@ -2,17 +2,17 @@ package gs.meetin.connector.dto;
 
 
 public class PinRequest {
-    final String email;
-    final String include_pin;
-    final String allow_register;
+    // Response
+    public ApiError error;
+
+    // Request
+    private String email;
+    private String include_pin;
+    private String allow_register;
 
     public PinRequest(String email) {
         this.email = email;
         this.include_pin = "1";
         this.allow_register = "1";
-    }
-
-    public class PinResult {
-        public ApiError error;
     }
 }
