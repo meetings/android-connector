b44595ea0534c59d2d2eec17775534c8ff2b0db6
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-22 12:04:00 +0300

    Add devicetype to login parameters

--- app/src/main/java/gs/meetin/connector/dto/LoginRequest.java
+++ app/src/main/java/gs/meetin/connector/dto/LoginRequest.java
@@ -9,10 +9,12 @@ public class LoginRequest {
     // Request
     private String email;
     private String pin;
+    private String deviceType;
 
     public LoginRequest(String email, String pin) {
         this.email = email;
         this.pin = pin;
+        this.deviceType = "android connector";
     }
 
     public class User {
