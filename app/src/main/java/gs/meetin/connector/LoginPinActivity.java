aa0b93060e8c3acf1acdcc4ed9b8033ed4be22d5
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-24 15:10:19 +0300

    sign in when enter is pressed

--- app/src/main/java/gs/meetin/connector/LoginPinActivity.java
+++ app/src/main/java/gs/meetin/connector/LoginPinActivity.java
@@ -5,7 +5,9 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -40,6 +42,7 @@ public class LoginPinActivity extends Activity {
 
         userEmail.setText(email);
 
+        setActionListeners();
         setButtonListeners();
     }
 
@@ -72,21 +75,35 @@ public class LoginPinActivity extends Activity {
         Dialogs.simpleAlert(this, event.getTitle(), event.getMessage()).show();
     }
 
+    private void setActionListeners() {
+        EditText inputPin = (EditText) findViewById(R.id.inputPin);
+
+        inputPin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEND) {
+                    signIn();
+                }
+                return true;
+            }
+        });
+    }
+
     private void setButtonListeners() {
         Button sendPin = (Button) findViewById(R.id.buttonSignInPin);
 
         sendPin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                view.setEnabled(false);
-                (findViewById(R.id.loginPinProgress)).setVisibility(View.VISIBLE);
-
                 signIn();
             }
         });
     }
 
     private void signIn() {
+        (findViewById(R.id.buttonSignInPin)).setEnabled(false);
+        (findViewById(R.id.loginPinProgress)).setVisibility(View.VISIBLE);
+
         String pin = ((EditText) findViewById(R.id.inputPin)).getText().toString();
 
         loginService.login(email, pin);
