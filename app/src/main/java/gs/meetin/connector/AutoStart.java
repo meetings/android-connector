b00bd001e2d811c7f18040d7cce42c5b9cba3b7d
Tuomas Lahti <tuomas.lahti@dicole.com>
2014-07-28 10:47:19 +0300

    Start service automatically on boot

---
+++ app/src/main/java/gs/meetin/connector/AutoStart.java
@@ -0,0 +1,22 @@
+package gs.meetin.connector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Listener for android.intent.action.BOOT_COMPLETED.
+ * Starts service on boot.
+ */
+public class AutoStart extends BroadcastReceiver
+{
+    public void onReceive(Context context, Intent i)
+    {
+        SessionManager sessionManager = new SessionManager(context);
+
+        if(sessionManager.isLoggedIn()) {
+            Intent serviceIntent = new Intent(context, ConnectorService.class);
+            context.startService(serviceIntent);
+        }
+    }
+}
